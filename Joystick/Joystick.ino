1e20c979659433935a62c6c952278d980098db17
bruce3152 <80897971+bruce3152@users.noreply.github.com>
2021-03-18 21:42:09 +0800

    Add files via upload

--- Joystick/Joystick.ino
+++ Joystick/Joystick.ino
@@ -1,1 +1,22 @@
+int sensorPin1 = A0;
+int sensorPin2 = A3;
+int sensorValue1 = 0;
+int sensorValue2 = 0;
+int sensorValue3 = 0;
 
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  sensorValue1 = analogRead(sensorPin1);
+  Serial.print("VRx:");
+  Serial.println(sensorValue1);
+  sensorValue2 = analogRead(sensorPin2);
+  Serial.print("VRy:");
+  Serial.println(sensorValue2);
+  int sensorValue3 = digitalRead(34);
+  Serial.print("SW:");
+  Serial.println(sensorValue3);
+  delay(300);
+}
