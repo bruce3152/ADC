167d09a19036678fd67f0bdbb5b5581f2a785efd
bruce3152 <80897971+bruce3152@users.noreply.github.com>
2021-03-18 21:58:27 +0800

    Add files via upload

--- MQ3/MQ3.ino
+++ MQ3/MQ3.ino
@@ -1,1 +1,16 @@
+void setup()
+{
+  Serial.begin(115200);
+  
+  analogSetPinAttenuation(A0,ADC_11db);
+  adcAttachPin(A0);
+  adcStart(A0);
+  while(analogread(A0)>600);
+}
 
+void loop()
+{
+  int alco;
+  alco = analogRead(A0);
+  Serial.println(alco);
+}
